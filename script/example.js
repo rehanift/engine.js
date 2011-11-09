98a5cc8deec451746b849e630b5432ae3b2eb4fe
Rehan Iftikhar <rehan.iftikhar@gmail.com>
2011-11-08 18:37:21 -0800

    these files are not needed

--- script/example.js
+++
@@ -1,6 +0,0 @@
-var engine = require("../engine").engine;
-
-var intake = engine.intake.create();
-var exhaust = engine.exhaust.create();
-var cylinder1 = engine.cylinder.create();
-var cylinder2 = engine.cylinder.create();
