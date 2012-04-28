25226193a97125f7a190418ac6f0bc147576fc34
Eric Allam <rubymaverick@gmail.com>
2012-04-28 17:35:46 -0400

    Call the eval event when a task is called without a console payload.  fixes #12

--- spec/engine/client_spec.js
+++ spec/engine/client_spec.js
@@ -69,6 +69,14 @@ describe("Client", function(){
 	    expect(mock_task.emit).toHaveBeenCalledWith("eval","foo bar");
 	});
 
+	it("emits an 'eval' event for the task eval if last_eval is undefined and console is undefined", function(){
+	    var mock_task = new mock.task();	    
+	    spyOn(client,'find_task_by_id').andReturn(mock_task);
+	    spyOn(mock_task,'emit');
+	    client.listening_socket.fakeSend('task-123 {"task_id":"task-123"}');
+	    expect(mock_task.emit).toHaveBeenCalledWith("eval",undefined);
+	});
+
 	// pending until we abstract out the sockets
 	xit("unsubscribes the listening socket when a task has been evaluated", function(){
 	    var mock_task = new mock.task();	    
