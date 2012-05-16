a6b9b80998889d8c2243bb2bbb6e3a8b7ffb5a3a
Rehan Iftikhar <rehan.iftikhar@gmail.com>
2012-05-16 08:40:05 -0700

    Fixed unit tests for ContextifyVm

--- spec/engine/piston/execution_strategies/contextify_spec.js
+++ spec/engine/piston/execution_strategies/contextify_spec.js
@@ -1,4 +1,5 @@
 var contextify_vm = require("../../../../lib/engine/piston/execution_strategies/contextify_vm").contextify_vm;
+var mock = require("../../../spec_helper").mock;
 
 describe("Code Runners", function(){
   describe("Contextify", function(){
@@ -7,21 +8,55 @@ describe("Code Runners", function(){
       this.sandbox = {
         add: function(a,b){ return a+b;}
       };
+      this.async = new mock.async_callback_register();
     });
 
     it("evalutes code against a sandbox", function(){
-      var response = this.strategy.execute("add(1,1)", this.sandbox);            
-      expect(response.evaluation).toBe(2);
+      var callback = jasmine.createSpy();
+      this.strategy.on("execution_complete", callback);
+      
+      this.strategy.execute("add(1,1)", this.sandbox, this.async);
+      this.async.emit("done");
+
+      waitsFor(function(){
+        return callback.callCount > 0;
+      });
+
+      runs(function(){
+        expect(callback.mostRecentCall.args[0].evaluation).toBe(2);
+      });
     });
 
     it("throws a SyntaxError when the user-code has bad syntax", function(){
-      var response = this.strategy.execute("add(1,1", this.sandbox);            
-      expect(response.evaluation).toContain("SyntaxError");
+      var callback = jasmine.createSpy();
+      this.strategy.on("execution_complete", callback);
+      
+      this.strategy.execute("add(1,1", this.sandbox, this.async);
+      this.async.emit("done");
+
+      waitsFor(function(){
+        return callback.callCount > 0;
+      });
+
+      runs(function(){
+        expect(callback.mostRecentCall.args[0].evaluation).toContain("SyntaxError");
+      });
     });
 
     it("throws a ReferenceError when the user-code calls an unknown sandbox function", function(){
-      var response = this.strategy.execute("subtract(1,1)", this.sandbox);
-      expect(response.evaluation).toContain("ReferenceError");
+      var callback = jasmine.createSpy();
+      this.strategy.on("execution_complete", callback);
+      
+      this.strategy.execute("subtract(1,1)", this.sandbox, this.async);
+      this.async.emit("done");
+
+      waitsFor(function(){
+        return callback.callCount > 0;
+      });
+
+      runs(function(){
+        expect(callback.mostRecentCall.args[0].evaluation).toContain("ReferenceError");
+      });
     });
   });
 
