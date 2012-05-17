7d46ab549e952e01ab44e0bc19a9494e78959f30
Rehan Iftikhar <rehan.iftikhar@gmail.com>
2012-05-16 19:27:25 -0700

    Fixed async callback register from firing prematurely

--- spec/helpers/async_spec.js
+++ spec/helpers/async_spec.js
@@ -8,10 +8,11 @@ describe("Async Callback Register", function(){
   it("emits a 'done' event when all callbacks have finished", function(){
     var callback = jasmine.createSpy();
     this.async.on('done', callback);
-    this.async.start();
-    this.async.start();
-    this.async.end();
-    this.async.end();
+    this.async.start(); // called in function body before callback
+    this.async.start(); // called in function body before callback
+    this.async.end(); // called immediately after synchronous evaluation
+    this.async.end(); // called in function body after callback has been called
+    this.async.end(); // called in function body after callback has been called
     expect(callback).toHaveBeenCalled();
   });
 
@@ -20,5 +21,15 @@ describe("Async Callback Register", function(){
     this.async.on("done", callback);
     this.async.end();
     expect(callback).toHaveBeenCalled();
+  });
+
+  it("does not emit a 'done' event when while a callback is in-progress", function(){
+    var callback = jasmine.createSpy();
+    this.async.on("done", callback);
+    this.async.start(); // called in function body before callback
+    this.async.end(); // called immediately after synchronous evaluation
+
+    // the callback has not finished yet
+    expect(callback).not.toHaveBeenCalled();
   });
 });
