8e0862fdaa07e966fe570e5a6615fae255fd5a24
Rehan Iftikhar <rehan.iftikhar@gmail.com>
2012-05-16 19:19:19 -0700

    Changed indentation from 4 spaces to 2

--- spec/helpers/async_spec.js
+++ spec/helpers/async_spec.js
@@ -1,24 +1,24 @@
 var async = require("../../lib/async");
 
 describe("Async Callback Register", function(){
-    beforeEach(function(){
-        this.async = async.create();
-    });
+  beforeEach(function(){
+    this.async = async.create();
+  });
 
-    it("emits a 'done' event when all callbacks have finished", function(){
-        var callback = jasmine.createSpy();
-        this.async.on('done', callback);
-        this.async.start();
-        this.async.start();
-        this.async.end();
-        this.async.end();
-        expect(callback).toHaveBeenCalled();
-    });
+  it("emits a 'done' event when all callbacks have finished", function(){
+    var callback = jasmine.createSpy();
+    this.async.on('done', callback);
+    this.async.start();
+    this.async.start();
+    this.async.end();
+    this.async.end();
+    expect(callback).toHaveBeenCalled();
+  });
 
-    it("emits a 'done' event when there are no callbacks", function(){        
-        var callback = jasmine.createSpy();
-        this.async.on("done", callback);
-        this.async.end();
-        expect(callback).toHaveBeenCalled();
-    });
+  it("emits a 'done' event when there are no callbacks", function(){        
+    var callback = jasmine.createSpy();
+    this.async.on("done", callback);
+    this.async.end();
+    expect(callback).toHaveBeenCalled();
+  });
 });
