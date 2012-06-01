acbf27cf2a46fe56b90d9d98f9e9d75c31ec8ae0
Rehan Iftikhar <rehan.iftikhar@gmail.com>
2012-05-31 18:10:16 -0700

    Removed node_vm. It has been fully replaced with contextify.

--- script/piston.js
+++ script/piston.js
@@ -1,20 +1,31 @@
 var engine = require('../engine').engine;
 
-console.log(process.argv);
+//////////////////////////////////////////////////////////////////////
+//          LEAVE THESE CHANGES TO THE FUNCTION PROTOTYPE           //
+//////////////////////////////////////////////////////////////////////
+
+Function.prototype.constructor = function(){ throw new SecurityError("The Function constructor may not be called"); };
+Function.prototype.toString = function(){ throw new SecurityError("'toString' may not be called on functions"); };
+
+var SecurityError = function(message){
+  this.message = message;
+  this.name = "SecurityError";
+};
+SecurityError.prototype = Error.prototype;
 
 var cylinder_id = process.argv[2];
 var console_endpoint = process.argv[3];
 
 // This is a service that responds to requests
 var piston = engine.piston.create({
-    listening_endpoint: "ipc:///tmp/"+cylinder_id+".ipc",
-    cylinder_id: cylinder_id,
-    console_endpoint: console_endpoint
+  listening_endpoint: "ipc:///tmp/"+cylinder_id+".ipc",
+  cylinder_id: cylinder_id,
+  console_endpoint: console_endpoint
 });
 
 process.on('SIGTERM', function(){
-    console.log("closing piston");
-    piston.close();
+  console.log("closing piston");
+  piston.close();
 });
 
 process.on("uncaughtException", function(err){
