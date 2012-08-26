e77b58bb3a895548b430dfd1192239cb383f02e9
Rehan Iftikhar <rehan.iftikhar@gmail.com>
2012-08-26 16:31:59 -0700

    Changed indentation from 4 spaces to 2

--- spec/engine/piston_spec.js
+++ spec/engine/piston_spec.js
@@ -2,32 +2,32 @@ var engine = require("../../engine").engine;
 var mock = require("../spec_helper").mock;
 
 describe("piston", function(){    
-    var piston = engine.piston.make({
-        execution_strategy: new mock.execution_strategy(),
-        console_socket: new mock.socket(),
-        sandbox_generator: new mock.sandbox_generator(),
-        server_socket: new mock.socket(),
-        result_socket: new mock.socket(),
-        watcher_socket: new mock.socket()
-    });
+  var piston = engine.piston.make({
+    execution_strategy: new mock.execution_strategy(),
+    console_socket: new mock.socket(),
+    sandbox_generator: new mock.sandbox_generator(),
+    server_socket: new mock.socket(),
+    result_socket: new mock.socket(),
+    watcher_socket: new mock.socket()
+  });
 
-    describe("#process_request", function(){
-        it("generates a new sandbox", function(){
-            spyOn(piston.sandbox_generator,'generate');
-            piston.process_request("{}");
-            expect(piston.sandbox_generator.generate).toHaveBeenCalled();
-        });
+  describe("#process_request", function(){
+    it("generates a new sandbox", function(){
+      spyOn(piston.sandbox_generator,'generate');
+      piston.process_request("{}");
+      expect(piston.sandbox_generator.generate).toHaveBeenCalled();
+    });
     
-        it("evaluates code against an execution strategy", function(){
-            spyOn(piston.execution_strategy,'execute');
-            piston.process_request("{}");
-            expect(piston.execution_strategy.execute).toHaveBeenCalled();
-        });
+    it("evaluates code against an execution strategy", function(){
+      spyOn(piston.execution_strategy,'execute');
+      piston.process_request("{}");
+      expect(piston.execution_strategy.execute).toHaveBeenCalled();
     });
+  });
 
-    it("#close closes all sockets", function(){
-        spyOn(piston.console_socket,'close');
-        piston.close();
-        expect(piston.console_socket.close).toHaveBeenCalled();
-    });
+  it("#close closes all sockets", function(){
+    spyOn(piston.console_socket,'close');
+    piston.close();
+    expect(piston.console_socket.close).toHaveBeenCalled();
+  });
 });
