9909a2f9f436dc75808d72ec3647d16c3babef53
Rehan Iftikhar <rehan.iftikhar@gmail.com>
2012-08-26 21:34:44 -0700

    WIP: Added a Connection for the PistonTaskReceiver

---
+++ spec/engine/piston/zmq_inbound_cylinder_connection_spec.js
@@ -0,0 +1,32 @@
+var ZmqInboundCylinderConnection = require("../../../lib/engine/cylinder/zmq_inbound_cylinder_connection");
+
+describe("ZmqInboundCylinderConnection", function(){
+  beforeEach(function(){
+    var context = require("zmq");
+    this.zmq_endpoint = "ipc:///tmp/zmq-inbound-cylinder-connection.ipc";
+
+    this.cylinder_push_socket = context.socket("push");
+    this.cylinder_push_socket.bindSync(this.zmq_endpoint);
+    
+    this.connection = ZmqInboundCylinderConnection.create({ zmq_endpoint: this.zmq_endpoint });
+  });
+
+  it("emits a 'message' event", function(){
+    var spy = jasmine.createSpy();
+    this.connection.on("message", spy);
+    
+    runs(function(){
+      this.cylinder_push_socket.send("foo");
+    });
+    
+    waitsFor(function(){
+      return spy.callCount > 0;
+    });
+
+    runs(function(){
+      expect(spy).toHaveBeenCalledWith("foo");
+      this.cylinder_push_socket.close();
+      this.connection.close();
+    });
+  });
+});
