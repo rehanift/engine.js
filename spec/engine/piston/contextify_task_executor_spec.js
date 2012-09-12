ac257582528f5ab4aed95132688ca24e0db0994f
Rehan Iftikhar <rehan.iftikhar@gmail.com>
2012-09-11 19:44:18 -0700

    WIP: Handle syntax and reference errors during task execution

--- spec/engine/piston/contextify_task_executor_spec.js
+++ spec/engine/piston/contextify_task_executor_spec.js
@@ -5,6 +5,24 @@ var TaskResponse = require("../../../lib/engine/client/task/response").TaskRespo
 describe("ContextifyTaskExecutor", function(){
   beforeEach(function(){
     this.executor = ContextifyTaskExecutor.create();
+    this.assert_task_response = function(task_request, task_response){
+      var spy = jasmine.createSpy();
+    
+      this.executor.on("task executed", spy);
+
+      runs(function(){
+        this.executor.execute_task(task_request);      
+      });
+
+      waitsFor(function(){
+        return spy.callCount > 0;
+      });
+
+      runs(function(){
+        expect(spy).toHaveBeenCalledWith(task_response);
+      });
+    };
+
   });
 
   it("executes a simple TaskRequest", function(){
@@ -15,20 +33,45 @@ describe("ContextifyTaskExecutor", function(){
       locals: {}
     });
     var simple_task_response = new TaskResponse({response:{evaluation:4}});
-    var spy = jasmine.createSpy();
-    
-    this.executor.on("task executed", spy);
 
-    runs(function(){
-      this.executor.execute_task(simple_task);      
-    });
-
-    waitsFor(function(){
-      return spy.callCount > 0;
-    });
-
-    runs(function(){
-      expect(spy).toHaveBeenCalledWith(simple_task_response);
-    });    
+    this.assert_task_response(simple_task, simple_task_response);
   });
+
+  it("executes a TaskRequest with locals", function(){
+    var simple_task = new TaskRequest({
+      task_id: "2",
+      context: "(function(locals){ return { hello:function(){ return 'hello ' + locals.world; } } })",
+      code: "hello()",
+      locals: { world:"world" }
+    });
+    var simple_task_response = new TaskResponse({response:{evaluation: "hello world"}});
+    
+    this.assert_task_response(simple_task, simple_task_response);
+  });
+
+  it("executes a TaskRequest that contains syntax errors", function(){
+    var simple_task = new TaskRequest({
+      task_id: "2",
+      context: "(function(locals){ return { hello:function(){ return 'hello'; } } })",
+      code: "hello(",
+      locals: { }
+    });
+    var simple_task_response = new TaskResponse({response:{error: "SyntaxError: Unexpected end of input"}});
+    
+    this.assert_task_response(simple_task, simple_task_response);
+  });
+
+  it("executes a TaskRequest that contains reference errors", function(){
+    var simple_task = new TaskRequest({
+      task_id: "2",
+      context: "(function(locals){ return { hello:function(){ return 'hello'; } } })",
+      code: "foo()",
+      locals: { }
+    });
+    var simple_task_response = new TaskResponse({response:{error: "ReferenceError: foo is not defined"}});
+    
+    this.assert_task_response(simple_task, simple_task_response);
+  });
+
+  xit("executes a TaskRequest with asynchronous code");  
 });
