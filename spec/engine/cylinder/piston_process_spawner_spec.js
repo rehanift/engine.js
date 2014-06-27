c371c23e6b55185a09f9a2866fd996b54f3f8028
Rehan Iftikhar <rehan.iftikhar@gmail.com>
2014-06-26 20:39:42 -0700

    A cylinder can spawn a piston process with a specified user and group

--- spec/engine/cylinder/piston_process_spawner_spec.js
+++ spec/engine/cylinder/piston_process_spawner_spec.js
@@ -1,11 +1,15 @@
 var PistonProcessSpawner = require("../../../lib/engine/cylinder/piston_process_spawner");
 var PistonProcess = require("../../../lib/engine/cylinder/piston_process");
 
+var uidNumber = require("uid-number");
+
 describe("PistonProcessSpawner",function(){
   beforeEach(function(){
     var spawner = PistonProcessSpawner.create({
       piston_script: __dirname + "/test_child_process.js",
-      script_args: []
+      script_args: [],
+      run_as_user: process.getuid(),
+      run_as_group: process.getgid()
     });
     this.process = spawner.spawn_new_process();
   });
@@ -14,6 +18,52 @@ describe("PistonProcessSpawner",function(){
     expect(this.process instanceof PistonProcess).toBeTruthy();
     this.process.kill();
   });
+
+  it("spawns a new node process running as the specified user and group", function(){
+    var nobody_uid = false, nobody_gid = false;
+    var callback = jasmine.createSpy();
+    var spawner;
+
+    runs(function(){
+      uidNumber("nobody", "nogroup", function(err, uid, gid){
+        nobody_uid = uid;
+        nobody_gid = gid;
+      });
+    });
+
+    waitsFor(function(){
+      return nobody_uid && nobody_gid;
+    });
+    
+    runs(function(){
+      spawner = PistonProcessSpawner.create({
+        piston_script: __dirname + "/test_child_process_dump_uid_gid.js",
+        script_args: [],
+        run_as_user: nobody_uid,
+        run_as_group: nobody_gid
+      });
+
+      this.process = spawner.spawn_new_process();
+      this.process.on('process error', callback);
+    });    
+
+    waitsFor(function(){
+      return callback.callCount > 0;
+    });
+
+    runs(function(){
+      var output_raw = callback.mostRecentCall.args[0];
+      var output_json = JSON.parse(output_raw);
+
+      expect(output_json.uid).toBe(nobody_uid);
+      expect(output_json.gid).toBe(nobody_gid);
+
+      this.process.kill();
+    });    
+
+  });
+
+  it("spawns a new node process running as the specified user and group");
 });
 
 describe("PistonProcess", function(){
@@ -21,7 +71,9 @@ describe("PistonProcess", function(){
     var callback = jasmine.createSpy();
     var spawner = PistonProcessSpawner.create({
       piston_script: __dirname + "/test_child_process.js",
-      script_args: []
+      script_args: [],
+      run_as_user: process.getuid(),
+      run_as_group: process.getgid()
     });
 
     runs(function(){
@@ -44,7 +96,9 @@ describe("PistonProcess", function(){
     var callback = jasmine.createSpy();
     var spawner = PistonProcessSpawner.create({
       piston_script: __dirname + "/test_child_process.js",
-      script_args: []
+      script_args: [],
+      run_as_user: process.getuid(),
+      run_as_group: process.getgid()
     });
 
     runs(function(){
@@ -66,7 +120,9 @@ describe("PistonProcess", function(){
     var callback = jasmine.createSpy();
     var spawner = PistonProcessSpawner.create({
       piston_script: __dirname + "/test_child_process.js",
-      script_args: []
+      script_args: [],
+      run_as_user: process.getuid(),
+      run_as_group: process.getgid()
     });
 
     runs(function(){
